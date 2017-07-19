851c6cf523ae78219634de29ca3a3f1489b5db96
Joe Huss <detain@interserver.net>
2017-07-19 05:45:59 -0400

    updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -27,14 +27,14 @@ class Plugin {
 
 	public static function getActivate(GenericEvent $event) {
 		$serviceClass = $event->getSubject();
-		if ($event['type'] == SERVICE_TYPES_VIRTUOZZO) {
+		if ($event['type'] == get_service_define('VIRTUOZZO')) {
 			myadmin_log(self::$module, 'info', 'Virtuozzo Activation', __LINE__, __FILE__);
 			$event->stopPropagation();
 		}
 	}
 
 	public static function getDeactivate(GenericEvent $event) {
-		if ($event['type'] == SERVICE_TYPES_VIRTUOZZO) {
+		if ($event['type'] == get_service_define('VIRTUOZZO')) {
 			myadmin_log(self::$module, 'info', self::$name.' Deactivation', __LINE__, __FILE__);
 			$serviceClass = $event->getSubject();
 			$GLOBALS['tf']->history->add(self::$module.'queue', $serviceClass->getId(), 'delete', '', $serviceClass->getCustid());
@@ -42,7 +42,7 @@ class Plugin {
 	}
 
 	public static function getChangeIp(GenericEvent $event) {
-		if ($event['type'] == SERVICE_TYPES_VIRTUOZZO) {
+		if ($event['type'] == get_service_define('VIRTUOZZO')) {
 			$serviceClass = $event->getSubject();
 			$settings = get_module_settings(self::$module);
 			$virtuozzo = new Virtuozzo(FANTASTICO_USERNAME, FANTASTICO_PASSWORD);
